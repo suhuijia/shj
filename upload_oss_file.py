54fdf883a76dd306d103909b880926919e7e7ba0
suhuijia <1484365363@qq.com>
2019-08-16 20:48:21 +0800

    Create upload_oss_file.py

---
+++ upload_oss_file.py
@@ -0,0 +1,18 @@
+import oss2
+import time
+
+
+def upload_ali_oss(path):
+    filename = time.strftime('%Y_%m_%d_%H%M%S_', time.localtime(time.time())) + path.split('/')[-1]
+    try:
+        auth = oss2.Auth('xxx', 'xxx')
+        bucket = oss2.Bucket(auth, 'oss-cn-hangzhou.aliyuncs.com', 'xxx')
+        bucket.put_object_from_file(filename, path)
+        return 'http://kousuanti.oss-cn-hangzhou.aliyuncs.com/' + filename
+    except Exception as e:
+        return None
+
+
+if __name__ == '__main__':
+    path = 'xxx/xxx/xxx.jpg'
+    url = upload_ali_oss(path)
